746747d41e5833552e3ababcdd63cc0acd1fbfc4
Tuba Demirkıran <tubademirkiran95@hotmail.com>
2017-12-03 15:23:28 +0300

    Business Layer

--- IKProjesi.BL/BusinessManager.cs
+++ IKProjesi.BL/BusinessManager.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using IKProjesi.BL.PersonelDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static IKProjesi.REP.Repositories;
 
 namespace IKProjesi.BL
 {
     public class BusinessManager
     {
+        public class PersonelManager : PersonelRepositories
+        {
+            public List<PersonelDto> PersonelDtoListele()
+            {
+                return GenelListe().Select(x => new PersonelDto
+                {
+                    PersonelId=x.PersonelId,
+                    PersonelAd=x.PersonelAd,
+                    PersonelSoyad=x.PersonelSoyad,
+                    Unvan=x.Unvan.Ad,
+                    IlceId=x.IlceId,
+                    IlId=x.Ilce.IlId
+                }).ToList();
+            }
+            public class UnvanManager : UnvanRepositories { }
+            public class EgitimManager : EgitimRepositories { }
+            public class IlManager : IlRepositories { }
+            public class BolumManager : BolumRepositories { }
+            public class IlceManager : IlceRepositories { }
+        }
     }
 }
---
+++ IKProjesi.BL/PersonelDTO/PersonelDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKProjesi.BL.PersonelDTO
+{
+    public class PersonelDto
+    {
+        public int PersonelId { get; set; }
+        public string PersonelAd { get; set; }
+        public string PersonelSoyad { get; set; }
+        public string Unvan { get; set; }
+        public int IlceId { get; set; }
+        public int IlId { get; set; }
+
+    }
+}
