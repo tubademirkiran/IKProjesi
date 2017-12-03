9389e1028ed838b17ef3d93e39aa9400da2712e5
Tuba Demirkıran <tubademirkiran95@hotmail.com>
2017-12-03 13:25:22 +0300

    zamanla olur :)

--- IKProjesi.REP/InterfaceRepository.cs
+++ IKProjesi.REP/InterfaceRepository.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IKProjesi.REP
 {
-    public class InterfaceRepository
+    interface InterfaceRepository<T> where T:class,new()
     {
+        DbSet<T> Set();//T bir class ve bu class veri tabanından bir tablo taşır.
+        void Sil(T entity); //entity T classından yani tablodan bir nesnedir
+        void Ekle(T entity);
+        T Bul(int id);
+        T Bul(string id);
+        void Guncelle(T entity);
+        void Save();
+        IQueryable<T> GenelListe();
+
     }
 }
