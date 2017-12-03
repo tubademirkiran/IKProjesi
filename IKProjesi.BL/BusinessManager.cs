4fd58140ea80cac7bb9ede635a3b735f201c9776
Tuba Demirkıran <tubademirkiran95@hotmail.com>
2017-12-03 14:50:04 +0300

    First

---
+++ IKProjesi.BL/BusinessManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKProjesi.BL
+{
+    public class BusinessManager
+    {
+    }
+}
