e9d5b8987286a9c03767611a4ba7ccd652dfdff7
Tuba Demirkıran <tubademirkiran95@hotmail.com>
2017-12-03 13:09:12 +0300

    First

---
+++ IKProjesi.REP/DbSingleTone.cs
@@ -0,0 +1,22 @@
+﻿using IKProjesi.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKProjesi.REP
+{
+    public class DbSingleTone
+    {
+        static IKContext db;
+        static public IKContext GetInstance()
+        {
+            if (db == null)
+            {
+                db = new DL.IKContext();
+            }
+            return db;
+        }
+    }
+}
